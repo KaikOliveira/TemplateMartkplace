f6339ef3daec334d9c162037a3636001e4be9d78
Michel Wene <michelwene@hotmail.com>
2021-12-09 22:30:42 -0300

    Ending Slide

--- src/components/Slide/index.jsx
+++ src/components/Slide/index.jsx
@@ -3,27 +3,72 @@ import Photo_shoes from "../../images/Photo_shoes.png";
 import Photo_Slipper from "../../images/Photo_Slipper.png";
 import Photo_boot from "../../images/Photo_boot.png";
 import Photo_belt from "../../images/Photo_belt.png";
+import Icon_arrow_left from "../../images/Icon_arrow_left.png";
+import Icon_arrow_right from "../../images/Icon_arrow_right.png";
+import Icon_star from "../../images/Icon_star.png";
+import Icon_two_star from "../../images/Icon_two_star.png";
+import Icon_three_star from "../../images/Icon_three_star.png";
+import Icon_four_star from "../../images/Icon_four_star.png";
 
 export const Slide = () => {
   return (
     <SlideContainer>
       <div>
-        <div>
-          <strong>Mais Vendidos</strong>
-        </div>
+        <strong>Mais Vendidos</strong>
+        <aside />
+      </div>
+      <section>
+        <aside className="left">
+          <img src={Icon_arrow_left} alt="Icone seta" />
+        </aside>
         <div>
           <img src={Photo_shoes} alt="Foto tênis social" />
+          <article>
+            <p>SAPATO FLOATER PRETO</p>
+            <img src={Icon_star} alt="Icone estrelas" />
+            <span>de R$ 279,00</span>
+            <b>por R$ 259,90</b>
+            <span>ou em 9x de R$ 28,87</span>
+            <button>COMPRAR</button>
+          </article>
         </div>
         <div>
           <img src={Photo_Slipper} alt="Foto Chinelo" />
+          <article>
+            <p>SANDÁLIA LINHO BROWN</p>
+            <img src={Icon_four_star} alt="Icone estrelas" />
+            <span>de R$ 299,00</span>
+            <b>por R$ 199,00</b>
+            <span>ou em 4x de R$ 49,75</span>
+            <button>COMPRAR</button>
+          </article>
         </div>
         <div>
           <img src={Photo_boot} alt="Foto botina" />
+          <article>
+            <p>BOTA MUSTANG PRETO</p>
+            <img src={Icon_two_star} alt="Icone estrelas" />
+            <span className="linha_riscado">de R$ 329,00</span>
+            <b>por R$ 299,90</b>
+            <span>ou em 10x de R$ 29,90</span>
+            <button>COMPRAR</button>
+          </article>
         </div>
         <div>
           <img src={Photo_belt} alt="Foto Cinto" />
+          <article>
+            <p>CINTO SEMICROMO PRETO 40MM</p>
+            <img src={Icon_three_star} alt="Icone estrelas" />
+            <span>de R$ 119,00</span>
+            <b>por R$ 79,90</b>
+            <span>ou em 10x de R$ 7.99</span>
+            <button>COMPRAR</button>
+          </article>
         </div>
-      </div>
+        <aside className="right">
+          <img src={Icon_arrow_right} alt="Icone seta" />
+        </aside>
+      </section>
     </SlideContainer>
   );
 };
--- src/components/Slide/styles.js
+++ src/components/Slide/styles.js
@@ -1,7 +1,93 @@
 import styled from "styled-components";
 
 export const SlideContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 1200px;
+  align-self: center;
 
+  div {
+    margin-top: 25px;
+    strong {
+    }
+    > aside {
+      width: 65px;
+      height: 3px;
+      margin-top: 5px;
+      background-color: #c0c0c0;
+    }
+  }
 
+  section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+    aside {
+      display: flex;
+      align-items: center;
+      position: absolute;
 
+      &.left {
+        left: -25px;
+      }
+      &.right {
+        right: -25px;
+      }
+    }
+    div {
+      width: 216px;
+      background-color: #fff;
+      img {
+        width: 216px;
+        height: 200px;
+      }
+
+      article {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px 0 15px 0;
+        justify-content: space-between;
+
+        p {
+          text-align: center;
+          font-size: 12px;
+          font-weight: 600;
+          color: #7a7a7a;
+        }
+        img {
+          display: flex;
+          width: 65px;
+          height: 10px;
+          align-self: center;
+          margin-bottom: 20px;
+        }
+
+        span {
+          color: #7a7a7a;
+
+          &.linha_riscado {
+            text-decoration: line-through;
+          }
+        }
+
+        b {
+          margin-bottom: 5px;
+        }
+
+        button {
+          color: #fff;
+          margin-top: 10px;
+          border-radius: 5px;
+          background-color: #000;
+          border: 0 none;
+
+          width: 126px;
+          height: 35px;
+        }
+      }
+    }
+  }
 `;
